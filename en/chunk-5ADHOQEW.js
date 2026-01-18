a32b815bc3812a16f57edd865962619449b4a696
beginwebdev2002 <beginwebdev2002@users.noreply.github.com>
2026-01-18 23:50:30 +0000

    deploy: 1396403f33451566d8b6e31b36781136f8043dfc

--- en/chunk-5ADHOQEW.js
+++
@@ -1,2 +0,0 @@
-import{I as n}from"./chunk-UIWLMSS7.js";var o={production:!0,BACKEND_URL:"https://aaa-api-2y6m.onrender.com"};var e=class t{get(r){return o[r]||""}static \u0275fac=function(a){return new(a||t)};static \u0275prov=n({token:t,factory:t.\u0275fac,providedIn:"root"})};export{e as a};
-/**i18n:53b94fc8a584fb0d28b70d52d4e35388c882dec0cc47e2f71218c9ceef5b3998*/
