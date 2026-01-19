c0ec008d8007d4cd706feb22e6e8c8fecdfd259f
beginwebdev2002 <beginwebdev2002@users.noreply.github.com>
2026-01-19 17:56:44 +0000

    deploy: 4e54e0f99de128d8860a602434d76447b80bbd8a

---
+++ ru/chunk-Q7QVOWCW.js
@@ -0,0 +1,2 @@
+import{I as r}from"./chunk-4OOI4HZV.js";var n={production:!0,BACKEND_URL:"https://aaa-api-2y6m.onrender.com",BACKEND_URL_ONLINE:"http://localhost:3000"};var e=class t{get(o){return n[o]||""}static \u0275fac=function(a){return new(a||t)};static \u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"})};export{e as a};
+/**i18n:b82b23be7f7338acfddc6e87193068a65d480bfebda2e4ddab7b7ca5af6f2821*/
