e6370605801d9af07b704c53dd3b8cbce4225c3c
beginwebdev2002 <beginwebdev2002@users.noreply.github.com>
2026-01-19 01:18:09 +0000

    deploy: bb1d633de0136f91ae24bfd4957f498a4c4ca16e

--- ru/chunk-MI5CZJWG.js
+++
@@ -1,2 +0,0 @@
-import{I as n}from"./chunk-LF5JYWEK.js";var o={production:!0,BACKEND_URL:"https://aaa-api-2y6m.onrender.com"};var e=class t{get(r){return o[r]||""}static \u0275fac=function(a){return new(a||t)};static \u0275prov=n({token:t,factory:t.\u0275fac,providedIn:"root"})};export{e as a};
-/**i18n:0aa8b7955aaa468e194734eb6a4a4ee16381a8cedba99aa173311fec5f57df07*/
