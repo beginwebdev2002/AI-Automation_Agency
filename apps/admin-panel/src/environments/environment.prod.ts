bf2a4cb73541c6e38f5a2bec5cdf5f5cd3e38200
Jamoliddin <jamoliddinqodirov18@gmail.com>
2026-01-20 12:05:27 +0500

    feat: Add environment configuration files for development and production backend URLs.

--- apps/admin-panel/src/environments/environment.prod.ts
+++ apps/admin-panel/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BACKEND_URL: 'https://aaa-api-2y6m.onrender.com',
+    BACKEND_URL: 'https://ai-automation-agency-j5qi.onrender.com',
     // BACKEND_URL_ONLINE: 'http://localhost:3000'
-    BACKEND_URL_ONLINE: 'https://aaa-api-2y6m.onrender.com'
+    BACKEND_URL_ONLINE: 'https://ai-automation-agency-j5qi.onrender.com'
 };
