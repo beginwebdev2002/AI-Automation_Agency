ce68c421f29d8c553745a0802c9176f18c4589e3
beginwebdev2002 <beginwebdev2002@users.noreply.github.com>
2026-01-23 20:30:31 +0000

    deploy: 5e03ef2fdf84d63167d6f683bd32c40033996007

--- ru/chunk-66DYX2NM.js
+++
@@ -1,2 +0,0 @@
-import{d as o}from"./chunk-ZUSBWXLF.js";import{I as n}from"./chunk-SUHW4BGF.js";var e=class t{get(r){return String(o[r]||"")}static \u0275fac=function(i){return new(i||t)};static \u0275prov=n({token:t,factory:t.\u0275fac,providedIn:"root"})};export{e as a};
-/**i18n:b82b23be7f7338acfddc6e87193068a65d480bfebda2e4ddab7b7ca5af6f2821*/
